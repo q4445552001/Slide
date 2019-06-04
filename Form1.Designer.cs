571cbbb77d813030d31a45b0ef7eb843b6fccb43
q4445552001 <42619163+q4445552001@users.noreply.github.com>
2019-06-04 17:51:41 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Slide
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.folderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.enableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(578, 424);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.folderToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(162, 88);
+            // 
+            // folderToolStripMenuItem
+            // 
+            this.folderToolStripMenuItem.Name = "folderToolStripMenuItem";
+            this.folderToolStripMenuItem.Size = new System.Drawing.Size(161, 28);
+            this.folderToolStripMenuItem.Text = "Folder";
+            this.folderToolStripMenuItem.Click += new System.EventHandler(this.folderToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.enableToolStripMenuItem,
+            this.disableToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(161, 28);
+            this.toolStripMenuItem1.Text = "Boundary";
+            // 
+            // enableToolStripMenuItem
+            // 
+            this.enableToolStripMenuItem.Checked = true;
+            this.enableToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.enableToolStripMenuItem.Name = "enableToolStripMenuItem";
+            this.enableToolStripMenuItem.Size = new System.Drawing.Size(155, 30);
+            this.enableToolStripMenuItem.Text = "Enable";
+            this.enableToolStripMenuItem.Click += new System.EventHandler(this.enableToolStripMenuItem_Click);
+            // 
+            // disableToolStripMenuItem
+            // 
+            this.disableToolStripMenuItem.Name = "disableToolStripMenuItem";
+            this.disableToolStripMenuItem.Size = new System.Drawing.Size(155, 30);
+            this.disableToolStripMenuItem.Text = "Disable";
+            this.disableToolStripMenuItem.Click += new System.EventHandler(this.disableToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(161, 28);
+            this.toolStripMenuItem2.Text = "Exit";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 424);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Slide";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem enableToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disableToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem folderToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Slide
+{
+    public partial class Form1 : Form
+    {
+        List<string> filename = new List<string> { };
+        Thread Slide;
+        int curr_x, curr_y;
+        bool isWndMove = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            disableToolStripMenuItem_Click(sender,e);
+            Form.CheckForIllegalCrossThreadCalls = false;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            this.FormBorderStyle = FormBorderStyle.None;
+            BackColor = Color.Lime;
+            TransparencyKey = Color.Lime;
+
+            this.Width = Properties.Settings.Default.FormW;
+            this.Height = Properties.Settings.Default.FormH;
+
+            //RegistryKey Reg = Registry.CurrentUser.OpenSubKey("Software", true);
+            /*if (!Reg.GetSubKeyNames().Contains("Slide"))
+                Reg.CreateSubKey("Slide");*/
+            //Registry.SetValue("HKEY_CURRENT_USER\\Software\\Slide", "path", path.SelectedPath, RegistryValueKind.String);
+            //Convert.ToString(Registry.GetValue("HKEY_CURRENT_USER\\Software\\Slide", "path", ""));
+            //Reg.Close();
+
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            if (!Directory.Exists(Properties.Settings.Default.path))
+            {
+                while (true)
+                {
+                    if (path.ShowDialog() == DialogResult.OK)
+                    {
+                        Properties.Settings.Default.path = path.SelectedPath;
+                        Properties.Settings.Default.Save();
+
+                        if (filename.Count() == 0)
+                        {
+                            filename.Clear();
+                            serchfile(path.SelectedPath);
+                        }
+
+                        if (filename.Count() != 0)
+                        {
+                            SlideTh(path.SelectedPath);
+                            break;
+                        }
+                    }
+                    else this.Close();
+                }
+            }
+            else
+            {
+                while (true)
+                {
+                    filename.Clear();
+                    serchfile(Properties.Settings.Default.path);
+                    if (filename.Count() == 0)
+                        if (path.ShowDialog() == DialogResult.OK)
+                        {
+                            Properties.Settings.Default.path = path.SelectedPath;
+                            Properties.Settings.Default.Save();
+                        }
+                        else break;
+
+                    if (filename.Count() != 0)
+                    {
+                        SlideTh(Properties.Settings.Default.path);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void serchfile(string path)
+        {
+            foreach (string file in Directory.GetDirectories(path))
+                serchfile(file);
+            foreach (string file in Directory.GetFiles(path))
+                if (file.ToLower().IndexOf(".jpg") != -1)
+                    filename.Add(file);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.curr_x = e.X;
+                this.curr_y = e.Y;
+                this.isWndMove = true;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.isWndMove)
+                this.Location = new Point(this.Left + e.X - this.curr_x, this.Top + e.Y - this.curr_y);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.isWndMove = false;
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            if (Slide != null && Slide.IsAlive)
+                Slide.Abort();
+            this.Close();
+        }
+
+        private void enableToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            enableToolStripMenuItem.Checked = true;
+            disableToolStripMenuItem.Checked = false;
+            if (!disableToolStripMenuItem.Checked && enableToolStripMenuItem.Checked)
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+        }
+
+        private void disableToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            enableToolStripMenuItem.Checked = false;
+            disableToolStripMenuItem.Checked = true;
+            if (disableToolStripMenuItem.Checked && !enableToolStripMenuItem.Checked)
+                this.FormBorderStyle = FormBorderStyle.None;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (Slide != null && Slide.IsAlive)
+                Slide.Abort();
+        }
+
+        private void folderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            while (true)
+                if (path.ShowDialog() == DialogResult.OK)
+                {
+                    if (Slide != null && Slide.IsAlive)
+                        Slide.Abort();
+                    Properties.Settings.Default.path = path.SelectedPath;
+                    Properties.Settings.Default.Save();
+
+                    if (filename.Count() == 0)
+                    {
+                        filename.Clear();
+                        serchfile(path.SelectedPath);
+                    }
+
+                    if (filename.Count() != 0)
+                    {
+                        SlideTh(path.SelectedPath);
+                        break;
+                    }
+                }
+                else break;
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.FormW = this.Width;
+            Properties.Settings.Default.FormH = this.Height;
+            Properties.Settings.Default.Save();
+        }
+
+        private void SlideTh(string path)
+        {
+            Slide = new Thread(() =>
+            {
+                while (true)
+                {
+                    if (filename.Count() == 0)
+                        serchfile(path);
+                    else
+                    {
+                        int rNumber = new Random().Next(0, filename.Count());
+                        pictureBox1.Image = Image.FromFile(filename[rNumber]);
+                        filename.RemoveAt(rNumber);
+                        Thread.Sleep(5000);
+                        pictureBox1.Image.Dispose();
+                    }
+                }
+            });
+            Slide.Start();
+        }
+    }
+}
